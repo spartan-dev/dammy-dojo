c32e5323885458721a69246eef2752899cb5262b
Salvador Emmanuel <s.e.jugr@gmail.com>
2023-08-21 20:20:08 -0600

    css basico done

---
+++ css_101/index.js
@@ -0,0 +1,13 @@
+/**
+ * Pseudo codigo es expresar la idea de el
+ * problema a resolver en palabras menos tecnicas o en
+ * bosquejs o ideas conceptuales
+ *
+ */
+
+/**
+ * pseudocdigo de selector javascript
+ *  $$$ selecciona por id mediente el index.js
+ * el objeto o nodo de id redbox2 y extraer el
+ * primer valor de su primer <p></p>
+ */
